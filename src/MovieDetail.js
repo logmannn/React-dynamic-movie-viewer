09d40c6daf57455eafe72ecf753b863abff53d94
lt <logantanous@gmail.com>
2018-08-16 10:37:13 -0500

    css changes

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Poster } from "./Movie";
+import Overdrive from "react-overdrive";
 
 const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 const BACKDROP_PATH = "http://image.tmdb.org/t/p/w1280";
@@ -26,16 +29,46 @@ class MovieDetail extends Component {
 
   render() {
     const { movie } = this.state;
+
     return (
-      <div>
-        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-        <h3>{movie.title}</h3>
-        <h3>{movie.release_date}</h3>
-        <p>{movie.overview}</p>
-      </div>
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+            <Poster
+              src={`${POSTER_PATH}${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </Overdrive>
+          <div>
+            <h3>{movie.title}</h3>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
     );
   }
 }
 
 export default MovieDetail;
+
+const MovieWrapper = styled.div`
+  position: relative;
+  padding-top: 50vh;
+  background-size: cover;
+  background: url(${props => props.backdrop}) no-repeat center top;
+`;
+
+const MovieInfo = styled.div`
+  background: white;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  > div {
+    margin-left: 20px;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
+import styled from "styled-components";
 import Movie from "./Movie";
 
-class MoviesList extends Component {
+class MoviesList extends PureComponent {
   state = {
     movies: []
   };
@@ -23,13 +24,20 @@ class MoviesList extends Component {
 
   render() {
     return (
-      <div>
+      <MovieGrid>
         {this.state.movies.map(movie => (
           <Movie key={movie.id} movie={movie} />
         ))}
-      </div>
+      </MovieGrid>
     );
   }
 }
 
 export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
